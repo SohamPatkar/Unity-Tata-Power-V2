07b69f2487a9a9847dd5c3343c8f0229b340e4d2
SohamPatkar <spatkar398@gmail.com>
2024-08-25 11:44:55 +0530

    Changed an AR Session script

--- Assets/Scripts/MaterialScript.cs
+++ Assets/Scripts/MaterialScript.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARCore;
 
 public class MaterialScript : MonoBehaviour
 {
-    [SerializeField]
-    public Material m_Material;
+    [SerializeField] public Material m_Material;
+    public GameObject _arSessionObject;
+    private ARSession _arSession;
     private float offsetX;
 
     // Start is called before the first frame update
     void Start()
     {
+        _arSession = _arSessionObject.GetComponent<ARSession>();
         offsetX = 0;
     }
 
@@ -25,6 +29,7 @@ public class MaterialScript : MonoBehaviour
 
     public void OnClickLocation()
     {
+        _arSession.Reset();
         SceneManager.LoadScene("LocationSelect");
     }
 }
